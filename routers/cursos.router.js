4820499f6dafa9ce49117ed4d54ac43c4929c1db
Cristian <calarcon@fi.uba.ar>
2025-12-09 20:17:13 -0300

    se agrega la ruta al nuevo metodo que filtra cursos

--- routers/cursos.router.js
+++ routers/cursos.router.js
@@ -4,10 +4,12 @@ import coursesController from '../controllers/cursos.controller.js'
 
 const router = express.Router();
 
-router.get('/', coursesController.getAllCourses);
+//router.get('/', coursesController.getAllCourses);
 router.get('/:id', coursesController.getCourseById);
 router.post('/', coursesController.getCourseById);
 router.put('/:id', coursesController.updateCourse)
 router.delete('/:id', coursesController.deleteCourseById);
+//reemplaza al get all
+router.get('/', coursesController.getCoursesByFilters);
 
 export default router;
