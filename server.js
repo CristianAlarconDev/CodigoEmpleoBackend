c462c320c971e33c02d22d87bff7280f8d49433d
Cristian <calarcon@fi.uba.ar>
2025-12-09 13:34:18 -0300

    se agrega import de rutas, conexion a db; ademas se usan las rutas para empleos y cursos

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 import express from "express";
 import "dotenv/config";
+import cors from 'cors';
+import connectDB from './src/config/db.js';
+import cursosRoutes from './routers/cursos.router.js';
+import empleosRoutes from './routers/empleos.router.js';
 
+//CONFIG
 const app = express();
 const PORT = process.env.PORT || 8000;
-
-
+//MIDDLEWARES
+app.use(cors())
+app.use(express.json());
+//RUTAS
+app.use('/api/cursos', cursosRoutes);
+app.use('/api/empleos', empleosRoutes);
+/*
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+})*/
 
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(` Server corriendo en http://localhost:${PORT}`);
+    });
+});
+
+/*
 app.listen(PORT, () => {
     console.log(`Server corriendo en puerto ${PORT}`)
 })
+    */
