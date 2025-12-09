e9037209b01039ed2cc07e0b182da3924364eb18
Cristian <calarcon@fi.uba.ar>
2025-12-09 19:44:43 -0300

    nuevo metodo se encarga de filtrar por palabra clave, seniority o modalidad; se reviso con queries directas en shell, devuelve la data filtrada por cantidad pedida junto a la pagina en la que esta, el limite usado y la cant total de documentos encontrados

--- services/empleos.service.js
+++ services/empleos.service.js
@@ -45,10 +45,48 @@ const actualizarEmpleo = async (id, empleoData) => {
         throw error;
     }
 };
+const obtenerEmpleosConFiltros = async (filtros = {}, pagina = 1, limite = 10, busqueda = "") => {
+    try {
+        const queryMongo = {};
+        //normalizo el campo porque no recuerdo como se escribio en el front 'i' para eso
+        if (filtros.modalidad) {
+            queryMongo.modalidad = { $regex: filtros.modalidad, $options: "i" };        }
+        if (filtros.seniority) {
+            queryMongo.seniority = { $regex: `^${filtros.seniority}$`, $options: "i" };
+        }
+        /*caso de que venga una palabra en el campo busqueda */
+        if (busqueda) {
+            queryMongo.$or = [{ titulo: { $regex: busqueda, $options: "i" } }, { empresa: { $regex: busqueda, $options: "i" } }];
+        }
+        const paginaActual = parseInt(pagina) || 1;
+        const limiteParseado = parseInt(limite) || 10;
+        const skip = (paginaActual - 1) * limiteParseado;
+        //para usar en collation y que haga comparacion sin case sensitive y sin tomar en cuenta tildes
+        const opcionesIdioma = { locale: 'es', strength: 1 };
+        const [empleos, total] = await Promise.all([
+            EmpleoModel.find(queryMongo).collation(opcionesIdioma).skip(skip).limit(limiteParseado),
+            EmpleoModel.countDocuments(queryMongo).collation(opcionesIdioma) 
+        ]);
+
+        return {
+            data: empleos,
+            meta: {
+                total,
+                page: paginaActual,
+                limit: limiteParseado,
+                totalPages: Math.ceil(total / limiteParseado)
+            }
+        };
+
+    } catch (error) {
+        throw error;
+    }
+}
+
 
 export default {
     crearEmpleo, actualizarEmpleo,
     borrarEmpleoPorId,
     obtenerEmpleoPorID,
-    obtenerEmpleos
+    obtenerEmpleos,obtenerEmpleosConFiltros
 }
